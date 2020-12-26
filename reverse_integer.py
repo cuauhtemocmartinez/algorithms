0f9f98a303ce5397aa5dee60a83185c221da9d33
Cuauhtemoc Alexander Martinez <65931652+cuauhtemocmartinez@users.noreply.github.com>
2020-12-25 17:40:47 -0800

    Add files via upload

---
+++ reverse_integer.py
@@ -0,0 +1,10 @@
+def reverse_order(x):
+    string = str(x)
+    
+    if string[0] == '-':
+        return int('-'+string[:0:-1])
+    else:
+        return int(string[::-1])
+    
+print(reverse_order(56))
+print(reverse_order(987))
